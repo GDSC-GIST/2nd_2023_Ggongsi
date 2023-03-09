59f67b9ae5a693e68df49484040d3122bad18610
aminor32 <aminor32dh@gmail.com>
2023-03-09 17:06:39 +0900

    Restrict accessible paths

--- Ggongsi-fe/src/App.js
+++ Ggongsi-fe/src/App.js
@@ -1,23 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useAtom } from "jotai";
 import Layout from "components/Layout";
 import QuestionPage from "pages/QuestionPage";
 import Main from "./Main";
 import Input from "./Input";
 import ResultPage from "pages/ResultPage";
+import { infoAtom } from "utils/atom";
 
 const App = () => {
-  /*   const url = "http://127.0.0.1:8000";
-  fetch(url)
-    .then((res: Response) => res.json())
-    .then((data) => console.log(data)); */
+  const [info] = useAtom(infoAtom);
+  const [studentID, setStudentID] = useState(0);
+
+  useEffect(() => {
+    setStudentID(info.studentID);
+  }, [info]);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Main />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+
           <Route path="/input" element={<Input />} />
-          <Route path="/question" element={<QuestionPage />} />
-          <Route path="/result" element={<ResultPage />} />
+          {studentID ? (
+            <>
+              <Route path="/question" element={<QuestionPage />} />
+              <Route path="/result" element={<ResultPage />} />
+            </>
+          ) : null}
         </Route>
       </Routes>
     </BrowserRouter>
